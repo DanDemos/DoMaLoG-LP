a17851825a46519c2bc8f4b25286e43d3562500f
Dan <dandemo14613@gmail.com>
2023-03-23 21:37:39 +0630

    save still writing show hide on phone, tablet, desktop

--- main.js
+++ main.js
@@ -7,5 +7,41 @@ $(document).ready(function(){
     $(this).next().show();
 
   }); 
-  
 })
+
+const mediaQueries = [
+  { name: 'phone', query: '(max-width: 575px)' },
+  { name: 'tablet', query: '(min-width: 576px) and (max-width: 991px)' },
+  { name: 'desktop', query: '(min-width: 992px)' },
+];
+
+const showOnPhone = document.querySelectorAll('.show-on-phone');
+const showOnTablet = document.querySelectorAll('.show-on-tablet');
+const showOnDesktop = document.querySelectorAll('.show-on-desktop');
+
+const handleMediaQueryChange = ({ name, matches }) => {
+  if (name === 'phone' && showOnPhone) {
+    showOnPhone.forEach(showOnPhone_element => {
+      matches ? showOnPhone_element.hidden = false : showOnPhone_element.hidden = true;
+    });
+  } else if (name === 'tablet' && showOnTablet) {
+    showOnTablet.forEach(showOnTablet_element => {
+      matches ? showOnTablet_element.hidden = false : showOnTablet_element.hidden = true;
+    });
+  } else if (name === 'desktop' && showOnDesktop) {
+    console.log(matches)
+    showOnDesktop.forEach(showOnDesktop_element => {
+      matches ? showOnDesktop_element.hidden = false : showOnDesktop_element.hidden = true;
+    });
+  }
+  console.log(`${name} is ${matches ? 'active' : 'inactive'}`);
+};
+
+mediaQueries.forEach(({ name, query }) => {
+  const mediaQuery = window.matchMedia(query);
+  handleMediaQueryChange({ name, matches: mediaQuery.matches });
+  mediaQuery.addEventListener('change', () => {
+    handleMediaQueryChange({ name, matches: mediaQuery.matches });
+  });
+});
+
