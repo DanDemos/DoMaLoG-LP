59a946724129b0d6c937ebaa5def55cc46b9bcdb
keinpyisi <38047639+keinpyisi@users.noreply.github.com>
2023-03-25 11:44:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,5 +7,33 @@ $(document).ready(function(){
     $(this).next().show();
 
   }); 
-  
 })
+
+const mediaQueries = [
+  { name: 'phone', query: '(max-width: 575px)' },
+  { name: 'tablet', query: '(min-width: 576px) and (max-width: 991px)' },
+  { name: 'desktop', query: '(min-width: 992px)' },
+];
+
+const handleMediaQueryChange = () => {
+  const elementsBySize = [
+    { size: 'phone', elements: document.querySelectorAll('.show-on-phone') },
+    { size: 'tablet', elements: document.querySelectorAll('.show-on-tablet') },
+    { size: 'desktop', elements: document.querySelectorAll('.show-on-desktop') }
+  ];
+
+  elementsBySize.forEach(({ size, elements }) => {
+    const otherSizes = elementsBySize.filter(s => s.size !== size);
+    elements?.forEach(element => {
+      const shouldShow = window.matchMedia(mediaQueries.find(q => q.name === size).query).matches || otherSizes.some(s => element.classList.contains(`show-on-${s.size}`) && window.matchMedia(mediaQueries.find(q => q.name === s.size).query).matches);
+      element.hidden = !shouldShow;
+    });
+  });
+};
+
+mediaQueries.forEach(({ query }) => {
+  const mediaQuery = window.matchMedia(query);
+  handleMediaQueryChange();
+  mediaQuery.addEventListener('change', handleMediaQueryChange);
+});
+
